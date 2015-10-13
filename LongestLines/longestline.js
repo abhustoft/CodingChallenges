4bfec310c1e0673a0020bf4964096b5ce006756a
Alf Bjørn <abhustoft@gmail.com>
2015-10-13 21:31:20 +0200

    Longest line challenge start

---
+++ LongestLines/longestline.js
@@ -0,0 +1,19 @@
+/**
+ * Created by abhustoft on 13.10.15.
+ */
+
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2],'utf8').split('\n').forEach(function (line) {
+    if (line != "") {
+
+        //Make array of array of each line
+        //Create indexing array: Map over array, create array of line sizes and index
+        //Sort indexing array
+        //Output the X longest lines
+
+
+
+     //console.log(answer_line);
+    }
+ });
