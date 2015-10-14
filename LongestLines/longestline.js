66eca3518a37eda1abcd74387fa093b0c8494359
Alf Bjørn <abhustoft@gmail.com>
2015-10-14 22:06:59 +0200

    Compacting

--- LongestLines/longestline.js
+++ LongestLines/longestline.js
@@ -1,25 +1,22 @@
 var fs  = require("fs");
 
 var noOfLines,
-    sorted,
     lineArray = [];
 
 fs.readFileSync(process.argv[2],'utf8').split('\n').forEach(function (line, lineNo) {
     if (line != "") {
 
-        debugger;
         if (lineNo === 0) {
             noOfLines = parseInt(line, 10);
             return;
         }
 
         //Make array of array of each line
-        //var aLine = line.split(' ');
         lineArray.push(line);
     }
  });
 
-var indexLine = function (line, index) {
+var indexLines = function (line, index) {
     return {'len': line.length,
         'pos': index};
 };
@@ -28,17 +25,8 @@ var byLength = function (a, b) {
     return b.len - a.len;
 };
 
-var printLongest = function (line, index) {
-    if (index < noOfLines) {
-        console.log(lineArray[sorted[index].pos]);
-    }
+var printLongest = function (line, index, sortedArray) {
+    index < noOfLines ? console.log(lineArray[sortedArray[index].pos]): true;
 };
 
-//Create indexing array: Map over array, create array of line sizes and index
-
-
-//Sort indexing array
-sorted = lineArray.map(indexLine).sort(byLength);
-
-//Output the X longest lines
-sorted.forEach(printLongest);
+lineArray.map(indexLines).sort(byLength).forEach(printLongest);
