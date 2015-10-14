87cefc447d6a0ba04a0fe994c44d7d4e862b0ea2
Alf Bjørn <abhustoft@gmail.com>
2015-10-14 20:40:19 +0200

    Working

--- LongestLines/longestline.js
+++ LongestLines/longestline.js
@@ -1,19 +1,36 @@
-/**
- * Created by abhustoft on 13.10.15.
- */
-
-
 var fs  = require("fs");
-fs.readFileSync(process.argv[2],'utf8').split('\n').forEach(function (line) {
+
+var noOfLines,
+    sorted,
+    lineArray = [];
+
+fs.readFileSync(process.argv[2],'utf8').split('\n').forEach(function (line, lineNo) {
     if (line != "") {
 
+        debugger;
+        if (lineNo === 0) {
+            noOfLines = parseInt(line, 10);
+            return;
+        }
+
         //Make array of array of each line
-        //Create indexing array: Map over array, create array of line sizes and index
-        //Sort indexing array
-        //Output the X longest lines
-
-
-
-     //console.log(answer_line);
+        //var aLine = line.split(' ');
+        lineArray.push(line);
     }
  });
+
+//Create indexing array: Map over array, create array of line sizes and index
+var ind = lineArray.map(function (line, index) {
+    return {'len': line.length,
+        'pos': index};
+});
+//Sort indexing array
+sorted = ind.sort(function (a, b) {
+    return b.len - a.len;
+});
+//Output the X longest lines
+sorted.forEach(function (line, index) {
+    if (index < noOfLines) {
+        console.log(lineArray[sorted[index].pos]);
+    }
+});
